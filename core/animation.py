67a18789edbdcc26e0e37e436243346605047ff2
Hennan Lewis <39320310+hennanlewis@users.noreply.github.com>
2026-05-20 13:48:19 -0300

    refactor: extract matplotlib artists into separate module

--- core/animation.py
+++ core/animation.py
@@ -1,169 +1,17 @@
 import matplotlib.pyplot as plt
-
 from matplotlib.animation import FuncAnimation
-from matplotlib.patches import Circle
-
 import numpy as np
-
 import math
 
 from core.epicycles import build_epicycles
+from core.artists import create_artists
+from core.canvas import setup_canvas
 
-FPS = 60
-ANIMATION_DURATION = 10
-ANIMATION_INTERVAL = 100 // FPS
-
-CANVAS_PADDING = 1.5
-
-ASPECT_RATIO = (16, 9)
-GRID_DIVISIONS = 20
-
-VECTOR_COLOR = (0, 0, 0, 0.15)
-FILL_COLOR = (0, 0, 0, 0.25)
-GRID_COLOR = (.55, .55, .55, 0.15)
-AXIS_COLOR = (1.0, 0.55, 0.1, 0.8)
-
-
-def setup_canvas(limit):
-    ratio_w, ratio_h = ASPECT_RATIO
-
-    fig_width = 10
-    fig_height = (
-        fig_width
-        * ratio_h
-        / ratio_w
-    )
-
-    fig, ax = plt.subplots(
-        figsize=(
-            fig_width,
-            fig_height
-        )
-    )
-
-    ax.set_aspect("equal")
-
-    ax.axis("off")
-
-    padded_limit = (
-        math.ceil(
-            (limit * CANVAS_PADDING)
-            / 100
-        ) * 100
-    )
-
-    half_width = padded_limit
-    half_height = (
-        padded_limit
-        * ratio_h
-        / ratio_w
-    )
-
-    ax.set_xlim(
-        -half_width,
-        half_width
-    )
-
-    ax.set_ylim(
-        half_height,
-        -half_height
-    )
-
-    grid_step = (
-        half_width * 2
-    ) / GRID_DIVISIONS
-
-    x_values = np.arange(
-        -half_width,
-        half_width + grid_step,
-        grid_step
-    )
-
-    y_values = np.arange(
-        -half_height,
-        half_height + grid_step,
-        grid_step
-    )
-
-    for x in x_values:
-        ax.axvline(
-            x,
-            linewidth=0.5,
-            color=GRID_COLOR,
-            zorder=0
-        )
-
-    for y in y_values:
-        ax.axhline(
-            y,
-            linewidth=0.5,
-            color=GRID_COLOR,
-            zorder=0
-        )
-
-    ax.axvline(
-        0,
-        linewidth=2,
-        color=AXIS_COLOR,
-        zorder=1
-    )
-
-    ax.axhline(
-        0,
-        linewidth=2,
-        color=AXIS_COLOR,
-        zorder=1
-    )
-
-    return fig, ax
-
-
-def create_artists(ax, num_harmonics):
-    vector_lines = []
-    circles = []
-
-    trace_line, = ax.plot(
-        [],
-        [],
-        linewidth=2,
-        color=VECTOR_COLOR,
-        alpha=1
-    )
-
-    trace_fill = ax.fill(
-        [],
-        [],
-        color=FILL_COLOR
-    )[0]
-
-    for _ in range(num_harmonics):
-        line, = ax.plot(
-            [],
-            [],
-            linewidth=1,
-            alpha=0.5
-        )
-
-        circle = Circle(
-            (0, 0),
-            0,
-            fill=False,
-            linewidth=1,
-            alpha=0.5
-        )
-
-        ax.add_patch(circle)
-
-        vector_lines.append(line)
-        circles.append(circle)
-
-    return (
-        vector_lines,
-        circles,
-        trace_line,
-        trace_fill
-    )
-
+from core.animation_config import (
+    FPS,
+    ANIMATION_DURATION,
+    ANIMATION_INTERVAL,
+)
 
 def run_animation(
     coeffs,
---
+++ core/artists.py
@@ -0,0 +1,53 @@
+from matplotlib.patches import Circle
+
+from core.animation_config import (
+    VECTOR_COLOR,
+    FILL_COLOR
+)
+
+
+def create_artists(ax, num_harmonics):
+    vector_lines = []
+    circles = []
+
+    trace_line, = ax.plot(
+        [],
+        [],
+        linewidth=2,
+        color=VECTOR_COLOR,
+        alpha=1
+    )
+
+    trace_fill = ax.fill(
+        [],
+        [],
+        color=FILL_COLOR
+    )[0]
+
+    for _ in range(num_harmonics):
+        line, = ax.plot(
+            [],
+            [],
+            linewidth=1,
+            alpha=0.5
+        )
+
+        circle = Circle(
+            (0, 0),
+            0,
+            fill=False,
+            linewidth=1,
+            alpha=0.5
+        )
+
+        ax.add_patch(circle)
+
+        vector_lines.append(line)
+        circles.append(circle)
+
+    return (
+        vector_lines,
+        circles,
+        trace_line,
+        trace_fill
+    )
